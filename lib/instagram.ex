9ab1560d0f37130166d3f99740eba82da600bcb4
Andrew Ostroumov <andrew.ostroumov@gmail.com>
2016-11-05 18:37:08 +0200

    Explore how to use module attributes

--- lib/instagram.ex
+++ lib/instagram.ex
@@ -1,5 +1,14 @@
 defmodule Instagram do
+  @moduledoc """
+  Provide interface to communicate with internal Instagram API
+  """
+
+  @default_instagram_id "Instagram ID"
+
+  @doc """
+  Return Instagram ID
+  """
   def get do
-    "Instagram ID"
+    @default_instagram_id
   end
 end
